462cc3db0898ce25d614b6e4152e8cd4ad273f0b
mddzzuhust <1539924369@qq.com>
2020-12-07 14:41:11 +0800

    leetcode

--- spring-demo1/src/main/java/com/javacode2018/HelloWorldController.java
+++ spring-demo1/src/main/java/com/javacode2018/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.javacode2018;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//请求URL:http://localhost:8080/helloWorld/index
 @RestController
 @RequestMapping("/helloWorld")
 public class HelloWorldController {
